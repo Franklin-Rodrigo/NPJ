222ebdabb2c6c00d5039568b1f8e02eb45bb5213
antoniofulg <antoniofulg@debian.fulgencio>
2019-11-11 17:03:10 -0300

    (Login): Agora o sistema informa ao aluno quando ele não está em dupla, e não pode realizar o login. (Icon): padronização do botão "sair"

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -7,8 +7,8 @@
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
         <button type="button" name="button" class="btn btn-sm btn-outline-danger" onClick="location.href='{{URL::to('Sair')}}'">
-          SAIR
           <span class="fas fa-sign-out-alt"></span>
+          SAIR
         </button>
       </li>
     </ul>
